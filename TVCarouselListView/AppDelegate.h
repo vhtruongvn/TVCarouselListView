c262fe35745bd4338fa19d6949cda3aeb1f976c3
Truong <vhtruongvn@gmail.com>
2015-04-29 21:01:07 +0800

    Initial Commit

---
+++ TVCarouselListView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TVCarouselListView
+//
+//  Created by Ray Vo on 29/4/15.
+//  Copyright (c) 2015 Truong Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TVCarouselListView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TVCarouselListView
+//
+//  Created by Ray Vo on 29/4/15.
+//  Copyright (c) 2015 Truong Vo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
